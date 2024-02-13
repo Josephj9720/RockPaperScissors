fcb6fc09473dc2f8bb99376cdd3dcfce4888f7e5
Josephj9720 <Josephj9720@gmail.com>
2024-02-13 14:37:50 -0500

    First Commit!

---
+++ src/main/java/com/josephj9720/rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.josephj9720.rockpaperscissors;
+
+/**
+ *
+ * @author josep
+ */
+public class RockPaperScissors {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
